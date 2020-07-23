7cd2af5587cc37011f6f177ce0e1d6a0b00c57ea
josieboy <josieboy55@gmail.com>
2020-07-23 15:21:28 +0300

    Update mailchimp.php

--- config/mailchimp.php
+++ config/mailchimp.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Instance\SystemSetting;
+
+$apikey = SystemSetting::where('key','mc_key')->get(['value'])->first();
+
 return [
-    'apikey' => env('MC_KEY'),
+    'apikey' => env($apikey),
 ];
